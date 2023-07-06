0b96907b9cb99f174e9dc938f5e9d4cbb4c456e4
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-06 07:36:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let container=document.querySelector("main#main")
+container.remove()
+let newHeader=document.createElement("h1")
+newHeader.id="victory"
+
+let yourName="Houstin Angwenyi"
+newHeader.textContent=`${yourName} is the champion`
+
+container.appendChild(newHeader)
